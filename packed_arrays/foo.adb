a575bb3ebdd37c9c28415d34c51f5e3b1c0a0b6d
Pierre-Marie de Rodat <derodat@adacore.com>
2014-09-30 13:37:56 +0200

    packed_arrays: add statically constrined packed arrays testing

--- packed_arrays/foo.adb
+++ packed_arrays/foo.adb
@@ -6,6 +6,10 @@ procedure Foo is
    type Packed_Array_Type is array (Natural range <>) of Item;
    pragma Pack (Packed_Array_Type);
 
+   type Bit_Field_Type is array (Natural range <>) of Boolean;
+   pragma Pack (Bit_Field_Type);
+   subtype Word_Bit_Field_Type is Bit_Field_Type (0 .. 15);
+
    A_Short : Packed_Array_Type := (-32, -1, 0, 1, 31);
    A_Long  : Packed_Array_Type :=
      (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15,
@@ -14,7 +18,14 @@ procedure Foo is
      (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15,
       16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31);
 
+   WBF : Word_Bit_Field_Type := (True, False, True, True, others => False);
+
    procedure Bar (A : Packed_Array_Type) is
+   begin
+      null;
+   end Bar;
+
+   procedure Bar (BF : Bit_Field_Type) is
    begin
       null;
    end Bar;
@@ -24,4 +35,5 @@ begin
    A_Long (1) := A_Long2 (1);
    A_Short (1) := A_Long2 (1);
    Bar (A_Long);
+   Bar (WBF);
 end Foo;
